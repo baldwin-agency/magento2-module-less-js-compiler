71a9c83c8955cbcfbee7e85db01b37ec5a96669d
Pieter Hoste <hoste.pieter@gmail.com>
2022-01-29 17:03:30 +0100

    Introduced a bunch of dev packages to check for coding standards and static analysis.

---
+++ .php-cs-fixer.php
@@ -0,0 +1,33 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__)
+    ->exclude('vendor')
+;
+
+$config = new PhpCsFixer\Config();
+return $config
+    ->setRules([
+        '@Symfony'               => true,
+        '@PSR12'                 => true,
+        'binary_operator_spaces' => ['default' => null, 'operators' => ['=>' => 'align']],
+        'concat_space'           => ['spacing' => 'one'],
+        'no_alias_functions'     => true,
+        'no_useless_sprintf'     => true,
+        'ordered_imports'        => [
+            'imports_order' => [
+                'class',
+                'function',
+                'const',
+            ],
+            'sort_algorithm' => 'alpha'
+        ],
+        'phpdoc_align'           => ['align' => 'left'],
+        'phpdoc_summary'         => false,
+        'self_accessor'          => true,
+        'single_line_throw'      => false,
+        'visibility_required'    => ['elements' => ['property', 'method']], // removed 'const' since we still support PHP 7.0 for now
+        'yoda_style'             => false,
+    ])
+    ->setFinder($finder)
+;
